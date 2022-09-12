a3ec439e326d343d68968f81ed38fb911b727b3c
Aurelius Natse <natseaurelius@gmail.com>
2022-09-12 14:54:23 -0700

    Print f.c

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this porogram prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a place of art,\n");
+	return (0);
+}
+
