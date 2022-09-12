a0ec23be7e6232c6bb05bc6f0d932ee9d04c20d7
Aurelius Natse <natseaurelius@gmail.com>
2022-09-12 01:03:06 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+	printf("Size of char: %zu bytes(s)\n", size of(char));
+	printf("Size of int: %zu bytes(s)\n", size of(int));
+	printf("Size of long int: %zu byte(s)\n", size of(long int));
+	printf("Size of float: %zu byte(s)\n", size of(float));
+
+	return 0;
+}
